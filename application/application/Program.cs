3781ce6e13e66a25e7755cae15222450e4fa8fec
FilipKupanovac <filip.kupanovac.11@gmail.com>
2022-10-25 17:42:23 +0200

    pozdrav iz aplikacije

---
+++ application/application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Nikoline Stjepanović!");
+        }
+    }
+}
